3dd44bf2c60eea622e86f381948169593e6027fb
Lizette <lbc@ucsb.edu>
2022-03-02 15:37:48 -0800

    wrote initial code for movies.cpp anf movies.h

--- movies.cpp
+++ movies.cpp
@@ -6,3 +6,24 @@
 using namespace std;
 
 #include "movies.h"
+
+// destructor
+Movie::~Movie() {}
+
+// getters
+string Movie::getMovie() const {
+    return movie;
+}
+
+int Movie::getScore() const {
+    return score;
+}
+
+// setters
+void Movie::setMovie(string newMovie) {
+    movie = newMovie;
+}
+
+void Movie::setScore(int newScore) {
+    score = newScore;
+}
--- movies.h
+++ movies.h
@@ -2,6 +2,11 @@
 // perm #4087193
 // 3/2/22
 
+/* TODO:
+    1. create class Movie
+    2. define constructors, destructors, etc
+*/
+
 #ifndef UTILITY_H
 #define UTILITY_H
 
@@ -9,7 +14,19 @@
 using namespace std;
 
 
-// code here
+class Movie {
+    public:
+        Movie(string mv = "", int sc = 0): movie(mv), score(sc) {}
+        ~Movie();
+        string getMovie() const;
+        int getScore() const;
+        void setMovie(string newSuit);
+        void setScore(int newNumber);
+        
+    private:
+        string movie;
+        int score;
+};
 
 
 #endif
